ce4970334c84ece9950985bd59d42e97473a50a0
Will Dirschka <wpd9@cornell.edu>
2020-11-23 02:09:16 -0500

    add plots

--- Server/plotTemps.py
+++ Server/plotTemps.py
@@ -1,7 +1,8 @@
 from sensor_plot import sensor_plot
 
-sensor = sensor_plot.plot(.1)
+sensor = sensor_plot.plot(5)
 sensor.add_sensor("Sensor 1", r'http://sensor1/temperature', "red")
-sensor.add_sensor("Sensor 2", r'http://sensor2/temperature', "green")
-sensor.add_sensor("Sensor 3", r'http://sensor3/temperature', "blue")
+sensor.add_sensor("Sensor 1 H", r'http://sensor1/humidity', "green")
+sensor.add_sensor("Sensor 2", r'http://sensor2/temperature', "yellow")
+sensor.add_sensor("Sensor 3", r'http://sensor3/temperature', "orange")
 sensor.run_forever()
